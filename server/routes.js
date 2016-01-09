d0faad4a8bfe1923792e299eb7d53917c2c3cf36
Daniel <dela1000@gmail.com>
2016-01-08 19:55:13 -0800

    [check] add test route

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+// var controllers = require('./controllers');
+var router = require('express').Router();
+
+  // returns a json
+  router.get('/', function (request, response) {
+    res.json({'home':'it'});
+  });
+
+module.exports = router;
