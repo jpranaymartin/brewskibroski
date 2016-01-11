b002d427f58a03ebabf0e96f9d2b26142e94ec42
Daniel <dela1000@gmail.com>
2016-01-11 10:39:53 -0800

    add routes and endpoints for front end

--- server/routes.js
+++ server/routes.js
@@ -1,42 +1,59 @@
-// var controllers = require('./controllers');
 var router = require('express').Router();
 var mysql = require('mysql');
 var parser = require('body-parser');
 var bcrypt = require('bcrypt-nodejs');
 var db = require('./db');
 var util = require('./utilities');
+var request = require('request');
+var session = require('express-session');
+var sequelize = require('sequelize');
 
-
-
-router.get('/', util.checkUser, function (request, response){
-  response.redirect('/app');
+db.User.findOrCreate({
+  where: {
+    username: 'daniel1',
+    password: 'test',
+    email: 'email'
+  }
 })
 
+//Open App
+router.get('/', util.checkUser, function (request, response) {
+  response.redirect('/app');
+});
+
 // User signup
-router.post('/auth', function (request, response){
+router.post('/authsignup', function (request, response){
+  console.log("request post: ", request.body)
   var username = request.body.username;
   var password = request.body.password;
   var email = request.body.email;
-
-  db.User.findOrCreate({
-    where: {
-      username: username,
-      password: password,
-      email: email
-    }
-  }).spread(function(result){
-    if(result){
-      console.log('Success!')
-      response.status(201).redirect('/app')
-    }
-  })
+  if (username !== null || password !== null || email !== null) {
+    db.User.findOrCreate({
+      where: {
+        username: username,
+        password: password,
+        email: email
+      }
+    }).spread(function(result, created){
+      if(created === true){
+        console.log('Success!')
+        util.createSession(request, response, username);
+        response.status(201).redirect('/app')
+      }else{
+        var dirname = __dirname;
+        dirname = dirname.slice(0, -6);
+        response.sendFile(dirname + '/client/auth.html');
+      }
+    })
+  }else{
+    response.sendStatus(401);
+  };
 })
 
 // User login
-router.get('/auth', function (request, response){
+router.post('/authlogin', function (request, response) {
   var username = request.body.username;
   var password = request.body.password;
-  var email = request.body.email;
 
   db.User.find({
     where: {
@@ -45,18 +62,19 @@ router.get('/auth', function (request, response){
     }
   }).then(function(result){
     if(result){
-      // start user session
-      util.createSession(request, response, username);
-      response.send(201);
+      util.createSession(request, response, result.id);
+      response.redirect(302, '/app');
     } else {
-      response.send(401);
+      response.sendStatus(401); //Handled on the front end
     }
   })
 })
 
 // Reroute to app
 router.get('/app', util.checkUser, function (request, response) {
-  response.sendFile(__dirname + '/app.html');
+  var dirname = __dirname
+  dirname = dirname.slice(0,-6)
+  response.sendFile(dirname + '/client/app.html');
 })
 
 // User logout
@@ -68,8 +86,131 @@ router.get('/logout', function (request, response) {
 
 // Reroute to login after logout
 router.get('/login', function (request, response) {
-  response.sendFile(__direname + '/auth.html')
+  var dirname = __dirname
+  dirname = dirname.slice(0, -6)
+  response.sendFile(dirname + '/client/auth.html')
 })
 
 
+//**TEST ROUTES**
+//TEST JSON RETURN
+router.get('/populateapp', function (request, response) { //NOT FULLY IMPLEMENTED. NOT SURE IF TWO CALLS ARE THE WAY TO GO
+  var username = request.body.username;
+  db.User.find({
+    where: {
+      id: '1' //or username: 'daniel1'
+    }
+  })
+  .then(function (result) {
+    response.json({result})
+  })
+})
+
+//Creating new event
+router.post('/events', function (request, response) {
+  var UserId = request.session.user;  //TO TEST WITH AUTH
+  var ownerLat = request.body.ownerLat;
+  var ownerLong = request.body.ownerLong;
+  var active = request.body.active;
+
+  if (UserId !== null || ownerLat !== null || ownerLong !== null || active !== null) {
+    db.Event.findOrCreate({
+      where: {
+        UserId: UserId, //Not being set for some reason.
+        ownerLat: ownerLat,
+        ownerLong: ownerLong,
+        active: active
+      }
+    }).spread(function(result){
+      if(result.$options.isNewRecord === true){
+        console.log('Success!')
+        response.status(201)
+        response.end()
+      }else{
+        console.log("Event already created")
+        response.status(404)
+        response.end()
+      }
+    })
+
+  }else{
+    console.log("Some or all incoming data is null")
+    response.status(404)
+    response.end()
+  };
+})
+
+
+//Accept event invite
+router.post('/eventaccept', function (request, response) {
+  var id = request.body.id;
+  var acceptedId = request.body.acceptedId;
+  var acceptedAt = Date.now();
+  var acceptedLat = request.body.acceptedLat;
+  var acceptedLong = request.body.acceptedLong;
+
+  db.Event.findById(id)
+  .then(function(acceptedEvent) {
+
+    if (acceptedEvent.active === true) {
+      var lat1 = acceptedEvent.ownerLat;
+      var lon1 = acceptedEvent.ownerLong;
+      var lat2 = acceptedLat;
+      var lon2 = acceptedLong;
+
+      //DEFINE CENTRAL LAT/LONG
+      var ownerPoints = [lat1,lon1]
+      var acceptedPoints = [lat2,lon2]
+      var centralLatLong;
+      centralLatLong = util.getCentralPoints(ownerPoints, acceptedPoints, 1)
+
+      console.log("acceptedEvent: ", JSON.stringify(acceptedEvent, null, "\t"));
+      acceptedEvent.acceptedId = request.session.user; // TO TEST
+      acceptedEvent.acceptedLat = acceptedLat;
+      acceptedEvent.acceptedLong = acceptedLong;
+      acceptedEvent.acceptedAt = acceptedAt;
+      acceptedEvent.centerLat = centralLatLong[0].x;
+      acceptedEvent.centerLong = centralLatLong[0].y;
+      acceptedEvent.accepted = true;
+      acceptedEvent.active = false;
+
+      console.log("acceptedEvent: ", JSON.stringify(acceptedEvent, null, "\t"));
+      acceptedEvent.save()
+      response.json(acceptedEvent)
+      response.end()
+    }else{
+      console.log("That event already expired, Brosephalus.")
+      response.status(406)
+      response.end()
+    };
+  });
+});
+
+
+//ADD ROUTE ADDFRIENDS, ADD ROUTE CHECKFRIENDS
+
+//Returns the events requested   // TO FINISH
+// router.post('/eventsreturn', function (request, response) {
+//   var id = request.body.id;
+//   db.Event.findAll({
+//     where: {
+//       id: id, // THIS SHOULD BE UserID AND WILL RETURN ALL THE ACTIVE EVENTS IN THE DB
+//       $and: {active: true}
+//     }
+//   }).then(function (results) {
+//     response.json({results})
+//     response.end()
+//   });
+// });
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
